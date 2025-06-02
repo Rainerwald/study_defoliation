4cfd0fc2c215001e97bf2570766f12315d1d6307
Rainerwald <Rainer.Hentschel@mailbox.org>
2025-06-02 13:35:19 +0200

    mod min

--- 02_model_defol.R
+++ 02_model_defol.R
@@ -129,25 +129,30 @@ for(ii in 1:length(ll))
 {
   ### data
   {
-    aa <-cc_trc[cc_trc$code_plot%in%ll[ii],];
-    aa <-aa[aa$code_removal<=9,]; aa <-aa[is.na(aa$code_defoliation)==F,]; aa <-aa[aa$code_defoliation>=0,]; 
-    a1 <-tapply(aa$code_defoliation,aa$survey_year,mean);
-    cc <-data.frame(year=as.integer(names(a1)),defol=a1);
-    b2 <-cc[,c("year","defol")]; b2$year <-b2$year+1; 
-    colnames(b2)[2] <-paste(c("defol"),"t1",sep="_");
-    cc <-merge(cc,b2,by="year",all.x=T);
+    ### defol
+    {
+      aa <-cc_trc[cc_trc$code_plot%in%ll[ii],];
+      aa <-aa[aa$code_removal<=9,]; aa <-aa[is.na(aa$code_defoliation)==F,]; aa <-aa[aa$code_defoliation>=0,]; 
+      a1 <-tapply(aa$code_defoliation,aa$survey_year,mean);
+      cc <-data.frame(year=as.integer(names(a1)),defol=a1);
+      b2 <-cc[,c("year","defol")]; b2$year <-b2$year+1; 
+      colnames(b2)[2] <-paste(c("defol"),"t1",sep="_");
+      cc <-merge(cc,b2,by="year",all.x=T);
+    }
     ### def.class
-    cc <-na.omit(cc);
-    cc[cc$defol_t1<=10,"def_c_t1"] <-0;
-    cc[cc$defol_t1>10 & cc$defol_t1<=25,"def_c_t1"] <-1;
-    cc[cc$defol_t1>25 & cc$defol_t1<=60,"def_c_t1"] <-2;
-    cc[cc$defol_t1>60 & cc$defol_t1<=99,"def_c_t1"] <-3;
-    cc[cc$defol_t1%in%100,"def_c_t1"] <-4;
+    {
+      cc <-na.omit(cc);
+      cc[cc$defol_t1<=10,"def_c_t1"] <-0;
+      cc[cc$defol_t1>10 & cc$defol_t1<=25,"def_c_t1"] <-1;
+      cc[cc$defol_t1>25 & cc$defol_t1<=60,"def_c_t1"] <-2;
+      cc[cc$defol_t1>60 & cc$defol_t1<=99,"def_c_t1"] <-3;
+      cc[cc$defol_t1%in%100,"def_c_t1"] <-4;
+    }
     ### l2m
     {
       bb <-l2m[[ll[ii]]]; # meteo
       if(is.null(nrow(bb))){next}
-      b1 <-bb[,c("year","YEAR-pr_MEANSUM","YEAR-pet_MEANSUM","0409-pr_MEANSUM","0409-pet_MEANSUM")]; # colnames(bb)
+      b1 <-bb[,c("year","YEAR-pr_MEANSUM","0409-pr_MEANSUM","YEAR-pet_MEANSUM","0409-pet_MEANSUM")]; # colnames(bb)
       b1$"YEAR-cwb_MEANSUM" <-b1$`YEAR-pr_MEANSUM`-b1$`YEAR-pet_MEANSUM`;
       b1$"0409-cwb_MEANSUM" <-b1$`0409-pr_MEANSUM`-b1$`0409-pet_MEANSUM`;
       b1$year <-as.integer(b1$year);
@@ -163,50 +168,62 @@ for(ii in 1:length(ll))
     }
     ### l2s -stress days
     {
-      bb <-l2s[["L2_miscday.txt"]];
-      bb <-bb[bb$Code_plot%in%ll[ii],];
-      ### sd_tt
-      dd <-bb[bb$STRES<=0.7,];
-      ee <-tapply(dd$STRES,dd$YR,function(x){length(x)})
-      ff <-data.frame(year=as.integer(names(ee)),sd_tt=ee)
+      bb <-l2s[["L2_calcday.txt"]]; # names(l2s)
+      bb <-bb[bb$code_plot%in%ll[ii],];  # summary(bb)
+      ### awat40
+      ee <-tapply(bb$awat40,bb$yr,function(x){mean(x)})
+      ff <-data.frame(year=as.integer(names(ee)),awat40=ee)
       cc <-merge(cc,ff,by="year",all.x=T);
-      cc[is.na(cc$sd_tt),"sd_tt"] <-0;
+      ### awat200
+      ee <-tapply(bb$awat200,bb$yr,function(x){mean(x)})
+      ff <-data.frame(year=as.integer(names(ee)),awat200=ee)
+      cc <-merge(cc,ff,by="year",all.x=T);
+      ### sd_ti
+      dd <-bb[bb$TI<0.7,];
+      ee <-tapply(dd$TI,dd$yr,function(x){length(x)})
+      ff <-data.frame(year=as.integer(names(ee)),sd_ti=ee)
+      cc <-merge(cc,ff,by="year",all.x=T);
+      cc[is.na(cc$sd_ti),"sd_ti"] <-0;
       ### sd_rew
-      dd <-bb[bb$RELAWAT<=0.4,];
-      ee <-tapply(dd$STRES,dd$YR,function(x){length(x)})
-      ff <-data.frame(year=as.integer(names(ee)),sd_rew=ee)
+      dd <-bb[bb$wetness60<=0.4,];
+      ee <-tapply(dd$wetness60,dd$yr,function(x){length(x)})
+      ff <-data.frame(year=as.integer(names(ee)),sd_rew40=ee)
       cc <-merge(cc,ff,by="year",all.x=T);
-      cc[is.na(cc$sd_rew),"sd_rew"] <-0;
-      #tt t1
-      b2 <-cc[,c("year","sd_tt","sd_rew")]; b2$year <-b2$year+1; 
-      colnames(b2)[2:3] <-paste(c("sd_tt","sd_rew"),"t1",sep="_");
-      cc <-merge(cc,b2,by="year",all.x=T);
+      cc[is.na(cc$sd_rew),"sd_rew40"] <-0;
+      ### t1
+      ee <-cc[,str_detect(colnames(cc),"year")|str_detect(colnames(cc),"awat")|str_detect(colnames(cc),"sd_")]; 
+      ee$year <-ee$year+1; 
+      colnames(ee)[2:ncol(ee)] <-paste(colnames(ee)[2:ncol(ee)],"t1",sep="_");
+      cc <-merge(cc,ee,by="year",all.x=T);
     }
     ###
     cc <-na.omit(cc);
   }
   ### corr-matrix
   {
-    G$d_temp <-file.path(G$d_out2,"corrplot"); if(!dir.exists(G$d_temp)){dir.create(G$d_temp)}
+    G$d_temp <-file.path(G$d_out2,"correlation"); if(!dir.exists(G$d_temp)){dir.create(G$d_temp)}
     graphics.off(); 
     out <-paste("corrplot",ll[ii],".png",sep="_");
     png(file.path(G$d_temp,out), units="mm", width=160, height=100, res=1000);
     cc_cor <-cor(cc[,-1])
     corrplot(cc_cor, method="color")
     graphics.off(); 
+    cc_cor <-sort(abs(cc_cor[,1]),decreasing = T); cc_cor <-cc_cor[-1];
+    cc_cor <-data.frame(var=names(cc_cor),cor=round(cc_cor,2));
+    out <-paste("corrtab",ll[ii],".csv",sep="_");
+    write.table(cc_cor,file.path(G$d_temp,out),col.names = T,row.names = F,sep=";",dec=".")
   }
   ### loop model
-  G$d_temp <-file.path(G$d_out2,"gam_mean_defoliation"); if(!dir.exists(G$d_temp)){dir.create(G$d_temp)}
-  dd <-sort(abs(cc_cor[,1]),decreasing = T);
-  mm <-names(dd)[2:6];
+  G$d_temp <-file.path(G$d_out2,"gam_plot"); if(!dir.exists(G$d_temp)){dir.create(G$d_temp)}
+  mm <-cc_cor[c(1:3),1];
   jj <-1; xx <-data.frame(matrix(NA,0,0))
   for(jj in 1:length(mm))
   {
     cc$y <-cc[,"defol"]; cc$x <-cc[,mm[jj]];
     ### nam
     nam <-paste0("gam_",mm[jj]);
-    nam_main <-paste0("defoliation ~",mm[jj])
-    nam_xlab <-expression(paste(mm[jj]))
+    nam_main <-paste0("defol ~ ",mm[jj])
+    nam_xlab <-mm[jj]
     nam_ylab <-expression(paste("mean defoliation [%]"))
     ### limit
     xmax <-ceiling(max(cc$x)); xmin <-ceiling(min(cc$x));
@@ -215,8 +232,6 @@ for(ii in 1:length(ll))
     {
       # https://stackoverflow.com/questions/67077306/plotting-output-of-gam-model
       ff <-gam(y~s(x,bs="cs"),method = "REML",dat=cc); # plot(ff)
-      # ff <-gam(y~s(x,bs="bs"),dat=cc); plot(ff)
-      # ff <-lm(y~splines::bs(x, 3),dat=cc); # plot(ff)
       gg <-summary(ff); gg$r.sq;
       preds <-predict(ff,se.fit=T)
       my_data <- data.frame(mu=preds$fit, 
@@ -239,9 +254,9 @@ for(ii in 1:length(ll))
         l_ciLine(mul = 5, colour = "blue", linetype = 2) + 
         l_points(shape = 19, size = 1, alpha = 0.1) + theme_classic()  +
         geom_text(aes(mean(b$model$x),max(b$model$y)*0.5), 
-                  label="y ~ s(x, bs = cs)",
+                  label="y ~ s(x, bs = cs, method = ´REML´)",
                   family="sans",parse=F,size = 5)+ 
-        labs(title=paste0("GAM Model: ",nam_main))
+        labs(title=paste0("GAM: ",nam_main))
       print(o); 
       ### save
       graphics.off(); 
@@ -251,7 +266,7 @@ for(ii in 1:length(ll))
       graphics.off(); 
       out <-paste("y",nam,ll[ii],".png",sep="_");
       png(file.path(G$d_temp,out), units="mm", width=160, height=100, res=1000);par(mar=c(3.5,3.5,2,1),mgp=c(2,.75,0),lab=c(nrow(aa)/5,12,7));
-      xtext <-xmax-100; ytext <-ymax;
+      xtext <-xmin+c(xmax-xmin)*0.1; ytext <-ymax-c(ymax-ymin)*0.1;
       pp <-ggplot()+
         geom_line(data = my_data, aes(x=x, y=mu), linewidth=1, col="blue")+
         geom_smooth(data=my_data,aes(ymin = low, ymax = high, x=x, y = mu), 
@@ -261,7 +276,7 @@ for(ii in 1:length(ll))
         labs(title=paste0("GAM Fit: ",nam_main)) + xlab(nam_xlab) + ylab(nam_ylab) +
         geom_text(aes(xtext,ytext), label=paste0("R-sq.(adj) = ",round(gg$r.sq,3)),
                   family="sans",parse=F,size = 5) +
-        geom_text(aes(xtext,ytext-ytext*0.1), label=paste0("dev.expl = ",round(gg$dev.expl,3)),
+        geom_text(aes(xtext,ytext-ytext*0.05), label=paste0("dev.expl = ",round(gg$dev.expl,3)),
                   family="sans",parse=F,size = 5);
       print(pp);
       ### save
@@ -270,72 +285,78 @@ for(ii in 1:length(ll))
     ### table
     {
       xx[jj,"var"] <-mm[jj]
-      xx[jj,"corr"] <-dd[mm[jj]]
+      xx[jj,"corr"] <-cc_cor[mm[jj],2]
       xx[jj,"r.sq"] <-gg$r.sq
       xx[jj,"dev.expl"] <-gg$dev.expl
     }
   }
   ### save table
   {
-    G$d_temp <-file.path(G$d_out2,"gam_mean_defoliation_table"); if(!dir.exists(G$d_temp)){dir.create(G$d_temp)};
+    G$d_temp <-file.path(G$d_out2,"gam_plot"); if(!dir.exists(G$d_temp)){dir.create(G$d_temp)};
     out <-paste("gam_table",ll[ii],".csv",sep="_");
     write.table(xx,file=file.path(G$d_temp,out),sep=";",dec=".",
                 col.names = T,row.names = F);
-  }
-  ### model multi
-  {
-    nam <-paste0("gam_","sx1_sx2");
-    cc$y <-cc[,"defol"]; # cc_cor[,1]
-    cc$x1 <-cc[,"defol_t1"]; 
-    # cc$x1 <-as.factor(cc[,"def_c_t1"]); 
-    cc$x2 <-cc[,"sd_tt"];
-    ff <-gam(y~x1+s(x2,k=4),method = "REML",dat=cc); # plot(ff)
-    # plot(ff,pages=2,scheme=1,unconditional=TRUE) 
-    gg <-summary(ff); gg;
-    preds <-predict(ff,se.fit=T)
   }
   ### single tree
   {
     aa <-cc_trc[cc_trc$code_plot%in%ll[ii],];
     aa <-aa[aa$code_removal<=9,]; aa <-aa[is.na(aa$code_defoliation)==F,]; aa <-aa[aa$code_defoliation>=0,]; 
     aa <-aa[,c("survey_year","tree_number","code_defoliation")];
+    colnames(aa) <-c("year","tree","defol");
     ### trees
-    bb <-gr_ipm[gr_ipm$code_plot%in%ll[ii],];
-    tt <-levels(as.factor(bb$tree_number));
-    kk <-3; cc_tree <-NULL;
+    hh <-gr_ipm[gr_ipm$code_plot%in%ll[ii],];
+    colnames(hh)[colnames(hh)=="tree_number"] <-"tree";
+    tt <-levels(as.factor(hh$tree));
+    tt <-tt[tt%in%aa$tree];
+    kk <-17; cc_tree <-NULL;
     for(kk in 1:length(tt))
     {
-      cc <-bb[bb$tree_number%in%tt[kk],];
-      dd <-aa[aa$tree_number%in%tt[kk],];
-      if(nrow(dd)==0){next}
-      gg <-lm(cc$diameter~cc$survey_year);
-      ee <-gg$coefficients[1]+gg$coefficients[2]*dd$survey_year;
-      ff <-data.frame(survey_year=dd$survey_year,dbh=ee);
-      dd <-merge(dd,ff,by="survey_year");   
+      cc <-hh[hh$tree%in%tt[kk],];
+      dd <-aa[aa$tree%in%as.integer(tt[kk]),];
+      if(nrow(dd)==1){next}
+      ### dbh
+      {
+        gg <-lm(cc$diameter~cc$survey_year);
+        ee <-gg$coefficients[1]+gg$coefficients[2]*dd$year;
+        ff <-data.frame(year=dd$year,dbh=ee);
+        dd <-merge(dd,ff,by="year"); 
+      }
+      ### defol
+      {
+        ### defol_t1 
+        b2 <-dd[,c("year","defol")]; 
+        b2$year <-b2$year+1; 
+        colnames(b2)[2] <-paste(colnames(b2)[2],"t1",sep="_");
+        dd <-merge(dd,b2,by="year"); 
+      }
       ### def.class
-      dd[dd$code_defoliation<=10,"def_c"] <-0;
-      dd[dd$code_defoliation>10 & dd$code_defoliation<=25,"def_c"] <-1;
-      dd[dd$code_defoliation>25 & dd$code_defoliation<=60,"def_c"] <-2;
-      dd[dd$code_defoliation>60 & dd$code_defoliation<=99,"def_c"] <-3;
-      dd[dd$code_defoliation%in%100,"def_c"] <-4;
-      ### def.class_t1 
-      b2 <-dd[,c("survey_year","def_c")]; 
-      b2$survey_year <-b2$survey_year+1; 
-      colnames(b2)[2] <-paste(c("def_c"),"t1",sep="_");
-      dd <-merge(dd,b2,by="survey_year",all.x=T);
-      ### sd_tt
-      hh <-l2s[["L2_miscday.txt"]];
-      hh <-hh[hh$Code_plot%in%ll[ii],];
-      ww <-hh[hh$STRES<=0.7,];
-      vv <-tapply(ww$STRES,ww$YR,function(x){length(x)})
-      uu <-data.frame(survey_year=as.integer(names(vv)),sd_tt=vv)
-      dd <-merge(dd,uu,by="survey_year",all.x=T);
-      dd[is.na(dd$sd_tt),"sd_tt"] <-0;
-      ### sd_tt_t1 
-      b2 <-dd[,c("survey_year","sd_tt")]; 
-      b2$survey_year <-b2$survey_year+1; 
-      colnames(b2)[2] <-paste(c("sd_tt"),"t1",sep="_");
-      dd <-merge(dd,b2,by="survey_year",all.x=T);
+      {
+        dd[dd$defol<=10,"def_c"] <-0;
+        dd[dd$defol>10 & dd$defol<=25,"def_c"] <-1;
+        dd[dd$defol>25 & dd$defol<=60,"def_c"] <-2;
+        dd[dd$defol>60 & dd$defol<=99,"def_c"] <-3;
+        dd[dd$defol%in%100,"def_c"] <-4;
+        ### def.class_t1 
+        b2 <-dd[,c("year","def_c")]; 
+        b2$year <-b2$year+1; 
+        colnames(b2)[2] <-paste(colnames(b2)[2],"t1",sep="_");
+        dd <-merge(dd,b2,by="year",all.x=T);
+      }
+      ### stress days
+      {
+        bb <-l2s[["L2_calcday.txt"]]; # names(l2s)
+        bb <-bb[bb$code_plot%in%ll[ii],];  # summary(bb)
+        ### awat200
+        ee <-tapply(bb$awat200,bb$yr,function(x){mean(x)})
+        ff <-data.frame(year=as.integer(names(ee)),awat200=ee)
+        dd <-merge(dd,ff,by="year",all.x=T);
+        ### sd_ti
+        gg <-bb[bb$TI<0.7,];
+        ee <-tapply(gg$TI,gg$yr,function(x){length(x)})
+        ff <-data.frame(year=as.integer(names(ee)),sd_ti=ee)
+        dd <-merge(dd,ff,by="year",all.x=T);
+        dd[is.na(dd$sd_ti),"sd_ti"] <-0;
+      }
       ### rbind
       cc_tree <-rbind(cc_tree,dd);
       ### na.omit
@@ -345,16 +366,16 @@ for(ii in 1:length(ll))
     {
       nam <-paste0("gam_single_tree");
       G$d_temp <-file.path(G$d_out2,nam); if(!dir.exists(G$d_temp)){dir.create(G$d_temp)};
-      cc_tree$y <-cc_tree[,"code_defoliation"]; # summary(cc_tree_tree)
-      cc_tree$x1 <-cc_tree[,"def_c_t1"]; 
-      cc_tree$x2 <-cc_tree[,"dbh"];
-      cc_tree$x3 <-cc_tree[,"sd_tt"];
-      cc_tree$x4 <-cc_tree[,"sd_tt_t1"];
-      ff <-gam(y~x1+s(x2,bs="cr",k=4)+s(x3,bs="cr",k=4)+s(x4,bs="cr",k=4),method = "REML",dat=cc_tree); summary(ff); plot(ff)
-      ff <-gam(y~x1+s(x3,bs="cr",k=4)+s(x4,bs="cr",k=4),method = "REML",dat=cc_tree); summary(ff); plot(ff)
-      ff <-gam(y~x1,method = "REML",dat=cc_tree); summary(ff); # plot(ff)
-      ff <-gam(y~s(x2,bs="cr",k=4),method = "REML",dat=cc_tree); summary(ff); plot(ff)
-      ff <-gam(y~s(x3,bs="cr",k=4)+s(x4,bs="cr",k=4),method = "REML",dat=cc_tree); summary(ff); plot(ff)
+      cc_tree$y <-cc_tree[,"defol"]; # summary(cc_tree_tree)
+      cc_tree$x1 <-cc_tree[,"defol_t1"]; 
+      cc_tree$x2 <-cc_tree[,"awat200"];
+      cc_tree$x3 <-cc_tree[,"sd_ti"];
+      cc_tree$x4 <-cc_tree[,"dbh"];
+      ff <-gam(y~s(x1,bs="cr",k=4)+s(x2,bs="cr",k=4)+s(x3,bs="cr",k=4)+s(x4,bs="cr",k=4),method = "REML",dat=cc_tree); summary(ff); #plot(ff)
+      #ff <-gam(y~x1+s(x3,bs="cr",k=4)+s(x4,bs="cr",k=4),method = "REML",dat=cc_tree); summary(ff); #plot(ff)
+      #ff <-gam(y~s(x1,bs="cr",k=4),method = "REML",dat=cc_tree); summary(ff); # plot(ff)
+      #ff <-gam(y~s(x2,bs="cr",k=4),method = "REML",dat=cc_tree); summary(ff); #plot(ff)
+      #ff <-gam(y~s(x3,bs="cr",k=4)+s(x4,bs="cr",k=4),method = "REML",dat=cc_tree); summary(ff); #plot(ff)
       gg <-summary(ff); gg;
       preds <-predict(ff,se.fit=T);
       my_data <- data.frame(mu=preds$fit, 
@@ -368,6 +389,28 @@ for(ii in 1:length(ll))
         print(gg);
         sink();
       }
+      ### plot fit 
+      {
+        # my_data <-my_data[order(my_data$mu),]
+        graphics.off(); 
+        out <-paste("y",nam,ll[ii],".png",sep="_");
+        # png(file.path(G$d_temp,out), units="mm", width=160, height=100, res=1000);par(mar=c(3.5,3.5,2,1),mgp=c(2,.75,0),lab=c(nrow(aa)/5,12,7));
+        xtext <-xmin+c(xmax-xmin)*0.1; ytext <-ymax-c(ymax-ymin)*0.1;
+        pp <-ggplot()+
+          geom_line(data = my_data, aes(x=y, y=mu), linewidth=1, col="blue")+
+          geom_smooth(data=my_data,aes(ymin = low, ymax = high, x=y, y = mu), 
+                      stat = "identity", col="green")+
+          geom_point(data=my_data,aes(x=y, y=y), col="black") +
+          # geom_smooth(method = gam, formula = y ~ splines::bs(x, 3), se = T) +
+          labs(title=paste0("GAM Fit: ",nam_main)) + xlab(nam_xlab) + ylab(nam_ylab) +
+          geom_text(aes(xtext,ytext), label=paste0("R-sq.(adj) = ",round(gg$r.sq,3)),
+                    family="sans",parse=F,size = 5) +
+          geom_text(aes(xtext,ytext-ytext*0.05), label=paste0("dev.expl = ",round(gg$dev.expl,3)),
+                    family="sans",parse=F,size = 5);
+        print(pp);
+        ### save
+        graphics.off(); 
+      } 
     }
   }
 }
